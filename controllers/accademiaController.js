3f63918e9a8e29598f651fb87fa0578b7915fd18
MatteoCancelli <matteo.cancelli.5@gmail.com>
2025-07-21 19:19:54 +0200

    aggiunta route pagina insegnanti , insegnanti.json e modifica json corsi

--- controllers/accademiaController.js
+++ controllers/accademiaController.js
@@ -6,36 +6,49 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export const mostraHome = (req, res) => {
-    res.render("accademia/index", {
-      title: "Accademia Musicale",
-      navbar: "partials/navbar-accademia",
-    });
-  };
-  
-export const mostraCorso = (req, res) => {
-  const id = req.query.id;
-  const dataPath = path.join(__dirname, "../data/corsi.json");
-  const corsi = JSON.parse(readFileSync(dataPath));
-
-  const corso = corsi.find(c => c.tag === id);
-
-  if (!corso) return res.status(404).render("404");
-
-  res.render("accademia/corso", { corso,
-  title: corso.titolo,
-  navbar: "partials/navbar-accademia" });
+  res.render("accademia/index", {
+    title: "Accademia Musicale",
+    navbar: "partials/navbar-accademia",
+  });
 };
 
-// export const mostraInsegnante = (req, res) => {
-//   const id = req.query.id;
-//   const dataPath = path.join(__dirname, "../data/insegnante.json");
-//   const corsi = JSON.parse(readFileSync(dataPath));
+export const mostraCorso = (req, res) => {
+  const id = req.query.id;
+  const corsiPath = path.join(__dirname, "../data/corsi.json");
+  const insegnantiPath = path.join(__dirname, "../data/insegnanti.json");
 
-//   const corso = corsi.find(c => c.tag === id);
+  const corsi = JSON.parse(readFileSync(corsiPath));
+  const insegnanti = JSON.parse(readFileSync(insegnantiPath));
 
-//   if (!corso) return res.status(404).render("404");
+  const corso = corsi.find(c => c.tag === id);
+  if (!corso) return res.status(404).render("404");
 
-//   res.render("accademia/insegnante", { insegnante,
-//   title: insegnante.nome,
-//   navbar: "partials/navbar-accademia" });
-// };
+  // Mappa tag → oggetti insegnanti
+  const insegnantiCorso = corso.insegnanti.map(tag =>
+    insegnanti.find(i => i.tag === tag)
+  );
+
+  res.render("accademia/corso", {
+    corso,
+    insegnanti: insegnantiCorso,
+    title: corso.titolo,
+    navbar: "partials/navbar-accademia"
+  });
+};
+
+
+export const mostraInsegnante = (req, res) => {
+  const id = req.query.id;
+  const dataPath = path.join(__dirname, "../data/insegnanti.json");
+  const insegnanti = JSON.parse(readFileSync(dataPath));
+
+  const insegnante = insegnanti.find(i => i.tag === id);
+
+  if (!insegnante) return res.status(404).render("404");
+
+  res.render("accademia/insegnante", {
+    insegnante,
+    title: insegnante.nome,
+    navbar: "partials/navbar-accademia"
+  });
+};
--- routes/accademia.js
+++ routes/accademia.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { mostraHome, mostraCorso } from "../controllers/accademiaController.js";
+import { mostraHome, mostraCorso, mostraInsegnante } from "../controllers/accademiaController.js";
 
 const router = express.Router();
 
 router.get("/", mostraHome);
 router.get("/corso", mostraCorso);
-// router.get("/insegnante", mostraInsegnante);
+router.get("/insegnante", mostraInsegnante);
 
 export default router;
