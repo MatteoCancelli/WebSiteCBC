b29f0782bec36a160a7793d15268cd78f45c3963
MatteoCancelli <matteo.cancelli.5@gmail.com>
2025-07-23 12:05:58 +0200

    fix

--- controllers/bandaController.js
+++ controllers/bandaController.js
@@ -1,6 +1,7 @@
 export const mostraHome = (req, res) => {
     res.render("banda/index", {
       title: "Corpo Bandistico di Castelcovati",
+      head: "partials/head-banda",
       navbar: "partials/navbar-banda",
     });
   };
@@ -8,6 +9,7 @@ export const mostraHome = (req, res) => {
   export const mostraEventi = (req, res) => {
     res.render("banda/eventi", {
       title: "Eventi",
+      head: "partials/head-banda",
       navbar: "partials/navbar-banda",
     });
   };
